8b1e00a4a9679aa10d1f91461fb535e1524defa6
NA-Silk <nicolasseda@gmail.com>
2024-08-16 04:31:09 -0400

    added total ratings

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -8,13 +8,16 @@ class ContentRating extends Component {
         this.state = {
             likes: 0, 
             dislikes: 0, 
+            totalRatings: 0, 
             handleLike:() => {
                 this.setState((prevState) => ({
-                    likes: prevState.likes + 1}));
+                    likes: prevState.likes + 1, 
+                    totalRatings: prevState.totalRatings + 1}));
             }, 
             handleDislike:() => {
                 this.setState((prevState) => ({
-                    dislikes: prevState.dislikes + 1}));
+                    dislikes: prevState.dislikes + 1, 
+                    totalRatings: prevState.totalRatings + 1}));
             }
         }
     }
@@ -33,6 +36,9 @@ class ContentRating extends Component {
                             <button className='dislike-button' onClick={this.state.handleDislike}>
                             Dislike ({this.state.dislikes})
                             </button>
+                            <p>
+                            Total Ratings: {this.state.totalRatings}
+                            </p>
                         </div>
                     </div>
                 </>
